b0f70cb700ec6e7dc103680b83da1072382c69e5
The Boss computer <sweawesome@gmail.com>
2019-07-28 00:09:01 +0200

    Ep.187 Fixed crashing, REMEMBER When using Log macros, use %f for floats, not %s

--- BattleTanks2/Source/BattleTanks2/Private/Tank.cpp
+++ BattleTanks2/Source/BattleTanks2/Private/Tank.cpp
@@ -8,7 +8,8 @@ ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	// we don't need to protect as added at construction
+	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
 }
 
 // Called when the game starts or when spawned
@@ -34,10 +35,11 @@ void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 void ATank::AimAt(FVector HitLocation)
 {
-
-	auto OurTankName = GetName();
-
-	UE_LOG(LogTemp, Warning, TEXT("%s aiming at %s"), *OurTankName, *HitLocation.ToString());
+	TankAimingComponent->AimAt(HitLocation, LaunchSpeed);
 
 }
 
+void ATank::SetBarrelReference(UStaticMeshComponent* BarrelToSet)
+{
+	TankAimingComponent->SetBarrelReference(BarrelToSet);
+}
